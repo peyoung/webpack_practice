9cfeaccb427d0137057f7da59514e9ac865b7dfc
inui <inui@conscious.jp>
2024-06-09 14:47:00 +0900

    Improve file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/javascripts/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "main.js",
+    filename: "javascripts/main.js",
   },
   module: {
     rules: [
@@ -24,9 +25,13 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/main.css",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+      inject: "body", // 独自差分修正　'body'を指定してスクリプトを<body>の最後に挿入
+    }),
+    new CleanWebpackPlugin(),
   ],
 };
