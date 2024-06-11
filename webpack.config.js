dfb2dcaabba678050b25bdff95ff0c0b84c9d66d
inui <inui@conscious.jp>
2024-06-11 14:00:30 +0900

    remove public Path option

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "javascripts/[name]-[contenthash].js",
-    publicPath: "/",
+    // publicPath: "/",
   },
   module: {
     rules: [
