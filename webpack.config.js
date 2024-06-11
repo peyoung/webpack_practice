57ef8f1b1a9a1490ae7b240391403d655e62ac02
inui <inui@conscious.jp>
2024-06-11 13:57:39 +0900

    Add hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: "./src/javascripts/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "javascripts/main.js",
+    filename: "javascripts/[name]-[contenthash].js",
     publicPath: "/",
   },
   module: {
@@ -65,7 +65,7 @@ module.exports = {
         test: /\.(png|jpg|jpeg)/,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext]",
+          filename: "images/[name]-[contenthash][ext]",
         },
         use: [
           // {
@@ -104,7 +104,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "./stylesheets/main.css",
+      filename: "./stylesheets/[name]-[contenthash].css",
     }),
     new HtmlWebpackPlugin({
       template: "./src/templates/index.pug",
