a9eef0f8052bb59d010483d1640cf268974e5635
inui <inui@conscious.jp>
2024-06-11 12:39:21 +0900

    Clean up project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   mode: "development",
@@ -14,6 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "javascripts/main.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -23,15 +23,6 @@ module.exports = {
         use: [
           {
             loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.vue/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "vue-loader",
           },
         ],
       },
@@ -64,6 +55,9 @@ module.exports = {
           },
           {
             loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
           },
         ],
       },
@@ -109,7 +103,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: "./stylesheets/main.css",
     }),
